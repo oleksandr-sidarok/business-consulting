dcd6e6b91b9786abb1dc3f28f6527433db3c95e2
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2021-11-11 19:11:11 +0200

    add task serve and refresh

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-const { src, dest } = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 const sourcemaps = require('gulp-sourcemaps')
 const csso = require('gulp-csso')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
 const rename = require('gulp-rename')
+const server = require('browser-sync');
 
 function html () {
   return src('src/*.html')
@@ -28,6 +29,24 @@ function cssNomin () {
     .pipe(dest('dist/css'))
 }
 
+function serve () {
+  server.init({
+    server: 'src/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  })
+  watch('src/scss/**/*.scss', series(css, cssNomin, refresh))
+  watch('src/*.html', series(html, refresh))
+}
+
+function refresh (done) {
+  server.reload()
+  done()
+}
+
 exports.html = html
 exports.css = css
 exports['css-nomin'] = cssNomin
+exports.serve = serve
