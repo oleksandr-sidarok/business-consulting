f05f7083b26027316ef9ac424a20125b5dcd8410
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2021-11-11 18:48:52 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest } = require("gulp");
+const sourcemaps = require('gulp-sourcemaps')
+const csso = require('gulp-csso')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+
+function html () {
+  return src('src/*.html')
+    .pipe(dest('dist'))
+}
+
+function css () {
+  return src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('dist/css'))
+}
+
+exports.html = html
+exports.css = css
