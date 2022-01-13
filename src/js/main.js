3510804e1a6c4bf06cf4be0bb383f54e09696f8e
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2022-01-13 11:47:56 +0200

    adaptive header

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 const details = document.querySelectorAll('details')
 const themeTogle = document.querySelector('.theme-toggle')
+const burgerBtn = document.querySelector('.burger')
+const navList = document.querySelector('.nav__list')
+const navCloseBtn = document.querySelector('.nav__close')
+
 details.forEach((el) => {
   summary = el.querySelector('summary')
   summary.addEventListener('click', () => {
-    console.log(el.hasAttribute('open'))
     if (!el.hasAttribute('open')) {
       el.classList.add('open')
     } else {
@@ -13,7 +16,14 @@ details.forEach((el) => {
 })
 
 themeTogle.addEventListener('click', () => {
-  console.log('ok!')
   document.body.classList.toggle('light')
   document.body.classList.toggle('dark')
+})
+
+burgerBtn.addEventListener('click', () => {
+  navList.classList.remove('hiden')
+})
+
+navCloseBtn.addEventListener('click', () => {
+  navList.classList.add('hiden')
 })
