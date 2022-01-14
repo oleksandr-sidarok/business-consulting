8a0049df3c2ae4e3cf23cfe9baa36c034d28cef5
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2022-01-14 18:00:39 +0200

    add project-slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,51 @@
+const projectSliderElement = document.querySelector('.project-slider')
+const projectSliderWrpper = projectSliderElement.querySelector('.swiper-wrapper')
+console.log(projectSliderElement)
+const projectSliderSettings = {
+  direction: 'horizontal',
+  loop: false,
+  allowTouchMove: false,
+  navigation: {
+    nextEl: '.project-button-next',
+    prevEl: '.project-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction'
+  }
+}
+
+const growthSliderSettings = {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+}
+
+const projectSlider = new Swiper('.project-slider', projectSliderSettings);
+const growthSlider = new Swiper('.growth-rates__slider', growthSliderSettings);
+
+if (document.documentElement.clientWidth < 1140) {
+  onProjectSlider(false)
+}
+if (document.documentElement.clientWidth > 1140) {
+  onProjectSlider(true)
+}
+
+window.addEventListener('resize', () => {
+  console.log('resize!')
+  if (document.documentElement.clientWidth < 1140) {
+    onProjectSlider(false)
+  }
+  if (document.documentElement.clientWidth > 1140) {
+    onProjectSlider(true)
+  }
+})
+
 const details = document.querySelectorAll('details')
 const themeTogle = document.querySelector('.theme-toggle')
 const burgerBtn = document.querySelector('.burger')
@@ -27,3 +75,13 @@ burgerBtn.addEventListener('click', () => {
 navCloseBtn.addEventListener('click', () => {
   navList.classList.add('hiden')
 })
+
+function onProjectSlider (bool) {
+  if (!bool) {
+    projectSliderWrpper.classList.remove('swiper-wrapper')
+    projectSlider.destroy(true, true)
+  } else {
+    projectSliderWrpper.classList.add('swiper-wrapper')
+    projectSlider.init(projectSliderSettings)
+  }
+}
